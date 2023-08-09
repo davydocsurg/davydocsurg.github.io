9ba2f197d5b83c597675c342222baa77b62a7b52
davydocsurg <davydocsurg@gmail.com>
2023-08-09 17:32:33 +0100

    33ccfbf

---
+++ _nuxt/a68e76a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{337:function(e,n){getCurrentYear=function(){var e=new Date;document.getElementById("currentYear").innerHTML=e.getFullYear()}}}]);
